b8d1f30854a10c6befabec5da0382c2cd31de503
orangain <orangain@gmail.com>
2015-02-08 11:51:24 +0900

    Status code is now configurable via env var STATUS_CODE
    As 301 redirect can be cached by browser, use 302 by default.
    It is recommended to use following codes:
    
    * Use 301 in production environment for SEO.
    * Use 302 in development environment to avoid unnecessary caching.

--- web.js
+++ web.js
@@ -6,14 +6,15 @@ if (!process.env.NEW_BASE_URL) {
 }
 
 var port = process.env.PORT || 5000;
+var statusCode = process.env.STATUS_CODE || 302;
 
 http.createServer(function(request, response) {
   var location = process.env.NEW_BASE_URL + request.url;
-  response.writeHead(302, {
+  response.writeHead(statusCode, {
     'Content-Type': 'text/plain',
     'Location': location
   });
-  console.log('[' + new Date().toISOString() + '] Redirecting to ' + location);
+  console.log('[' + new Date().toISOString() + '] ' + statusCode + ' Redirecting to ' + location);
   response.end();
 }).listen(port);
 
